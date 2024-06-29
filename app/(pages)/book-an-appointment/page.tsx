3e6256626b3b451dd8aeed9317bb70d5cba53003
Fabian Ajokubi <70838932+fabiconcept@users.noreply.github.com>
2024-06-29 02:24:52 +0100

    Rename book-an-appointment page to BookAppointmentPage

--- app/(pages)/book-an-appointment/page.tsx
+++ app/(pages)/book-an-appointment/page.tsx
@@ -6,7 +6,7 @@ import BookAppointment from "@/app/HomePage Sections/BookAppointment";
 export const metadata: Metadata = { ...generalMetadata, title: "Book an Appointment | AmeriCare Atlanta" };
 
 
-export default function ThankYouPage() {
+export default function BookAppointmentPage() {
     return (
         <div className='max-2xl:pt-32 pt-24 py-8'>
             <NavigationComponent />
