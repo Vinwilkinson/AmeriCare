69e0eb86b148a54aae9588c26e0cec9509e880c0
Fabian Ajokubi <70838932+fabiconcept@users.noreply.github.com>
2024-07-10 15:40:00 -0400

    Add book-an-appointment to site map

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -32,5 +32,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
             changeFrequency: 'monthly',
             priority: 0.3,
         },
+        {
+            url: 'https://americareinhome.com/book-an-appointment',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 1,
+        },
     ]
 }
